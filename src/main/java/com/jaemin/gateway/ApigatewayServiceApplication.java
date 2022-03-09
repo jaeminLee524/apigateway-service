09ce94d5948ddcedffb0f47e55d13e6a5e44a08b
jaeminLee524 <rkwoals524@hanmail.net>
2022-03-10 00:15:48 +0900

    Initial commit

---
+++ src/main/java/com/jaemin/gateway/ApigatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jaemin.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApigatewayServiceApplication.class, args);
+    }
+
+}
